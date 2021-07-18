df92e17ad68112fca3ef0f1571f90d0cb9dc9f8a
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-07-18 16:43:37 +0200

    add function that allow players to add marks to a specific spot on the board

--- script.js
+++ script.js
@@ -18,16 +18,37 @@ const gameBoard = (()=>{
         [1,4,7],
         [2,5,8]
     ];
-
-    const playerMark = (function(){
+    
+    //empty arrays for pushing values from players
+    let choosenArray1 = [];
+    let choosenArray2 = [];
+    
+    //get our marks from players - X or O
+    const getPlayerMark = (function(){
         const cell = document.querySelectorAll('.cell');
+        const player1 = document.querySelector('#player1');
+        const player2 = document.querySelector('#player2');
+        let choosenBtn;
+        player1.addEventListener('click',() => choosenBtn = 'player1');
+        player2.addEventListener('click', () => choosenBtn = 'player2');
         cell.forEach((cell)=>{
             cell.addEventListener('click',()=>{
-                cell.textContent = "X"
+                if(choosenBtn == 'player1'){
+                    cell.textContent = 'X';
+                    choosenArray1.push(cell.id);
+                    console.log(choosenArray1);
+                }
+                else if(choosenBtn == 'player2'){
+                    cell.textContent = 'O';
+                    choosenArray2.push(cell.id);
+                    console.log(choosenArray2);
+                }
+                                    
             })
         })
-    })();   
-
+    })();
+    
+       
 })();
 
  
