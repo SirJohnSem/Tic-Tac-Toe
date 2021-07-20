fe0c89b916902304c5826b57b741a45f6475347f
SirJohnSem <jewgienijdanilczik@mail.ru>
2021-07-20 11:20:46 +0200

    add restart the game logic

--- script.js
+++ script.js
@@ -12,63 +12,78 @@ const gameBoard = (()=>{
     let choosenArray2 = [];
     let commonArray = [];
     
-    //get our marks from players - X or O
-    const getPlayerMark = (function(){
+    const gamePlay = (function(){
         const cell = document.querySelectorAll('.cell');
         const start = document.querySelector('.startBtn');
+        const restart = document.querySelector('.restartBtn')
         const showResult = document.querySelector('.showResult');
         let choosenBtn;
         
-        start.addEventListener('click',() => choosenBtn = player1.status);
-        cell.forEach((cell)=>{
-            cell.addEventListener('click',()=>{
-                if(choosenBtn == player1.status && cell.textContent == ''){
-                    cell.textContent = player1.mark;
-                    commonArray.push(cell.id);
-                    choosenArray1.push(parseInt(cell.id));
+        //start game by clicking START button
+        const startGame = (()=>{
+            start.addEventListener('click',() => choosenBtn = player1.status);
+            cell.forEach((cell)=>{
+                cell.addEventListener('click',()=>{
                     
-                    //show draw result if commonArray is full
-                    if(getDrawResult()){
-                        showResult.textContent =  "It's DRAW!"                         
-                    };
-                    
-                    //show player1 as a winner
-                    if(compareArrays(choosenArray1)){
-                        showResult.textContent =  "Player 1 is the WINNER!"                        
-                    };
-                    
-                    //change player1 turn to player2 turn
-                    player1.status = false;
-                    player2.status = true;
-                                                                                
-                }
-                else if(choosenBtn == player2.status && cell.textContent == ''){
-                    cell.textContent = player2.mark;
-                    commonArray.push(parseInt(cell.id));
-                    choosenArray2.push(parseInt(cell.id));
-                    
-                    //show draw result if commonArray is full
-                    if(getDrawResult()){
-                        showResult.textContent =  "It's DRAW!"                         
-                    };
-                    
-                    //show player2 as a winner
-                    if(compareArrays(choosenArray2)){
-                        showResult.textContent =  "Player 2 is the WINNER!"                        
-                    };
-                    
-                    //change player2 turn to player1 turn
-                    player1.status = true;
-                    
-                                    
-                }
+                    //get display mark from player1
+                    if(choosenBtn == player1.status && cell.textContent == ''){
+                        cell.textContent = player1.mark;
+                        commonArray.push(cell.id);
+                        choosenArray1.push(parseInt(cell.id));
+                        
+                        //show draw result if commonArray is full
+                        if(getDrawResult()){
+                            showResult.textContent =  "It's DRAW!"                         
+                        };
+                        
+                        //show player1 as a winner
+                        if(compareArrays(choosenArray1)){
+                            showResult.textContent =  "Player 1 is the WINNER!"                        
+                        };
+                        
+                        //change player1 turn to player2 turn
+                        player1.status = false;
+                        player2.status = true;
+                                                                                    
+                    }
+    
+                    //get display mark from player2
+                    else if(choosenBtn == player2.status && cell.textContent == ''){
+                        cell.textContent = player2.mark;
+                        commonArray.push(parseInt(cell.id));
+                        choosenArray2.push(parseInt(cell.id));
+                        
+                        //show draw result if commonArray is full
+                        if(getDrawResult()){
+                            showResult.textContent =  "It's DRAW!"                         
+                        };
+                        
+                        //show player2 as a winner
+                        if(compareArrays(choosenArray2)){
+                            showResult.textContent =  "Player 2 is the WINNER!"                        
+                        };
+                        
+                        //change player2 turn to player1 turn
+                        player1.status = true;
+                        
+                    }
+                })
             })
-        })
-    })();
+        })();
+
+        //restart game by clicking RESTART button
+        const restartGame = (function(){
+            restart.addEventListener('click',()=>{
+               window.location.reload();
+            })
+        })();
+        
+})();
     
 
     //compare arrays from players with possible win combinations
     const compareArrays =(function(getArray){
+
         //possible win combinations
         const winArray = {  
             one: [0,1,2],
@@ -81,7 +96,7 @@ const gameBoard = (()=>{
             eight: [2,5,8]
         }   
         
-        //start compare arrays, when player check at least 3 marks
+        //start compare arrays, when player checked at least 3 marks
         if(getArray.length>2){
             if(winArray.one.every((a)=>(getArray.includes(a)))){
                 return true
@@ -111,12 +126,13 @@ const gameBoard = (()=>{
     });
 
     //check commonArray length
-    function getDrawResult(){
+    const getDrawResult = (function(){
         if(commonArray.length===9){
             return true;
         }
-    }
-    
+    });
+
+       
       
 })();
 
